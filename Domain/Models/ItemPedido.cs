3550bf6cc48e20f41179a46c14f3798b2069187a
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-20 01:28:39 -0300

    Criando item Pedido e Usuario Model

---
+++ Domain/Models/ItemPedido.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GerenciamentoDePedidos.Domain.Models
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+        public Produto Produto { get; set; }
+    }
+}
---
+++ Domain/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using GerenciamentoDePedidos.Enums;
+
+namespace GerenciamentoDePedidos.Domain.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do usuário")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o login do usuário")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Digite o e-mail do usuário")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe o perfil do usuário")]
+        public PerfilEnum? Perfil { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha do usuário")]
+        public string Senha { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
