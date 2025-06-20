74d37562360d548838b1972f666c082343432847
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-20 01:33:33 -0300

    Add a View Home do Projeto, Enum e DbContext

---
+++ Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace GerenciamentoDePedidos.Data
+{
+    public class DbContext
+    {
+        private readonly IConfiguration _config;
+        private readonly string _connectionString;
+
+        public DbContext(IConfiguration config)
+        {
+            _config = config;
+            _connectionString = _config.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Enums/PerfilEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciamentoDePedidos.Enums
+{
+    public enum PerfilEnum
+    {
+        Admin = 1,
+        Usuario = 2
+    }
+}
