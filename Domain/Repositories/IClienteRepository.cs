1577427aec6d61b2e3929fb10da3596d1bad0c36
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-20 01:29:03 -0300

    Criando o Repository de Pedido e Cliente

---
+++ Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoDePedidos.Domain.Models;
+using System.Collections.Generic;
+
+namespace GerenciamentoDePedidos.Domain.Repositories
+{
+    public interface IClienteRepository
+    {
+        IEnumerable<Cliente> GetAll();
+        Cliente GetById(int id);
+        void Create(Cliente cliente);
+        void Update(Cliente cliente);
+        void Delete(int id);
+        IEnumerable<Cliente> Search(string termo);
+    }
+}
---
+++ Domain/Repositories/IPedidoRepository.cs
@@ -0,0 +1,13 @@
+﻿using GerenciamentoDePedidos.Domain.Models;
+using System.Collections.Generic;
+
+namespace GerenciamentoDePedidos.Domain.Repositories
+{
+    public interface IPedidoRepository
+    {
+        IEnumerable<Pedido> GetAll(int? clienteId, string status);
+        Pedido GetById(int id);
+        int Create(Pedido pedido);
+        void UpdateStatus(int id, string status);
+    }
+}
