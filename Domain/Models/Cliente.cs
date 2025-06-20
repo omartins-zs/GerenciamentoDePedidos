f130bdc94964d7f1878a9200773bed3240c55dde
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-20 01:27:53 -0300

    Criando Cliente Model

---
+++ Domain/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciamentoDePedidos.Domain.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
