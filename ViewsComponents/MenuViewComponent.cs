c27fc1e8a7793b46c7670ac7dc053351738daf42
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-20 01:32:08 -0300

    Criando as Views de Shared e Components de Menu e Navbar

---
+++ ViewsComponents/MenuViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GerenciamentoDePedidos.Domain.Models;
+using GerenciamentoDePedidos.Helper;
+
+namespace GerenciamentoDePedidos.ViewComponents
+{
+    [ViewComponent(Name = "Menu")]
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly ISessao _sessao;
+        public MenuViewComponent(ISessao sessao) => _sessao = sessao;
+
+        public IViewComponentResult Invoke()
+        {
+            var usuario = _sessao.BuscarSessaoDoUsuario();
+            return View(usuario);
+        }
+    }
+}
