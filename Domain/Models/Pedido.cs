7f865c1467eb4725b9d0f8d6755358f2acbe9fa9
omartins-zs <gabrielmatheus209@gmail.com>
2025-06-20 01:28:03 -0300

    Criando Pedido Model

---
+++ Domain/Models/Pedido.cs
@@ -0,0 +1,13 @@
+﻿namespace GerenciamentoDePedidos.Domain.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public DateTime DataPedido { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string Status { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<ItemPedido> Itens { get; set; }
+    }
+}
